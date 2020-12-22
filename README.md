8219bca02770b2e3d470fdfc3b5ef5fba3727ebf
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2020-12-22 18:57:42 +0300

    Update 22-12-2020

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sergey Registry Extension (SRE)
+**namespace SergeyRegistryExtension**
+## English
+**Registry.AssociateExtension** - association of a file extension with a program  
+**Registry.RemoveAssociateExtension** - removing the association of a file extension with a program  
+## Русский
+**Registry.AssociateExtension** - ассоциация расширения файла с программой  
+**Registry.RemoveAssociateExtension** - удаление ассоциации расширения файла с программой  
---
+++ SergeyRegistryExtension/Registry.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+
+namespace SergeyRegistryExtension
+{
+    public static class Registry
+    {
+        private const string prefix = "SG_";
+
+        /// <summary>
+        /// Ассоциация расширения файла с программой
+        /// </summary>
+        /// <param name="expansion">Расширение</param>
+        /// <param name="productName">Имя продукта</param>
+        /// <param name="pathApp">Путь к exe приложения</param>
+        /// <param name="pathIcon">Путь к иконке файла (Добускается значение null или string.Empty)</param>
+        public static void AssociateExtension(string expansion, string productName, string pathApp, string pathIcon)
+        {
+            RegistryKey reg = null;
+            try
+            {
+                reg = Microsoft.Win32.Registry.CurrentUser;
+                reg = reg.OpenSubKey("Software\\Classes", true);
+                reg.CreateSubKey(expansion).SetValue("", $"{prefix}{productName}");
+                reg = reg.CreateSubKey($"{prefix}{productName}");
+                if (string.IsNullOrEmpty(pathIcon) == false)
+                    reg.CreateSubKey("DefaultIcon").SetValue("", $"\"{pathIcon}\"");
+                reg = reg.CreateSubKey("Shell");
+                reg = reg.CreateSubKey("Open");
+                reg.CreateSubKey("Command").SetValue("", $"\"{pathApp}\" \"%1\"");
+            }
+            finally
+            {
+                if (reg != null)
+                    reg.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Удаление ассоциации расширения файла с программой
+        /// </summary>
+        /// <param name="expansion">Расширение</param>
+        /// <param name="productName">Имя продукта</param>
+        public static void RemoveAssociateExtension(string expansion, string productName)
+        {
+            RegistryKey reg = null;
+            try
+            {
+                reg = Microsoft.Win32.Registry.CurrentUser;
+                reg = reg.OpenSubKey("Software\\Classes", true);
+                reg.OpenSubKey(expansion, true).DeleteValue($"{prefix}{productName}", false);
+                reg.DeleteSubKeyTree($"{prefix}{productName}", false);
+            }
+            finally
+            {
+                if (reg != null)
+                    reg.Dispose();
+            }
+        }
+    }
+}
