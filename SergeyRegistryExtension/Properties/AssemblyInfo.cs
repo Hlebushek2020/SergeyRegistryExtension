518e9798b56c112df6695a2b5c66b447ff429a4a
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2020-12-24 10:27:32 +0300

    Update 24-12-2020

--- SergeyRegistryExtension/Properties/AssemblyInfo.cs
+++ SergeyRegistryExtension/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SergeyRegistryExtension")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Sergey Govorunov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
